894971bd0cd6c7a5f632535da75e8fb7938a015d
Patrick Solis <psolis00@gmail.com>
2018-08-31 10:11:40 -0700

    fixed minor bugs

--- portfolio.js
+++ portfolio.js
@@ -6,19 +6,23 @@ function scroll_to_div(div_id)
         scrollTop: $("#"+div_id).offset().top
     },
     'slow');
-    if(window.innerWidth <= 768)
+    if(window.innerWidth <= 992)
     {
         hide_nav();
     }
 }
 
 function show_nav(){
-    if(window.innerWidth <= 768)
+    if(window.innerWidth >= 1200)
     {
-        document.getElementById("nav").style.width = "100%";
+        document.getElementById("nav").style.width = "20%";
+    }
+    else if(window.innerWidth >= 768)
+    {
+        document.getElementById("nav").style.width = "35%";
     }
     else{
-        document.getElementById("nav").style.width = "20%";
+        document.getElementById("nav").style.width = "100%";
     }
 }
 
